e6c508bfa2017f8a496d25f294fb49e8b28251be
Tomáš Kalina <info@tomaskalina.cz>
2020-05-25 08:07:04 +0200

    Update installApp.sh

--- installApp.sh
+++ installApp.sh
@@ -31,7 +31,7 @@ cp .zshrc ~/
 #set zsh to default
 chsh -s /bin/zsh
 
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs build-essential
 
 sudo npm i -g yarn
